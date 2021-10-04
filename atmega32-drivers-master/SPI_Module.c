7bff747cb7923a0b2a2b34beda6d8314104e54c4
abd elwahed dardeer <89838156+Eldardeery@users.noreply.github.com>
2021-10-04 14:48:40 +0200

    Update SPI_Module.c

--- atmega32-drivers-master/SPI_Module.c
+++ atmega32-drivers-master/SPI_Module.c
@@ -1,62 +1,122 @@
 /*
- * SPI_Module.c
+ * ATMEGA32_SPI.c
  *
- *  Created on: Sep 5, 2021
- *      Author: dardeery
- */
+ * Created: 9/30/2021 11:59:56 AM
+ *  Author: dardeery
+ */ 
 
-#include"SPI_Module.h"
 
-void Spi_Init(SPI_OP_MODE MODE)
+#include "ATMEGA32_SPI.h"
+
+
+void SPI_Init(SPI_Configuration_Struct * SPI_Object)
 {
-	if(MODE == SPI_MSTR)
+	SPSR =0;
+	SPDR =0;
+	switch (SPI_Object->spi_Conrtol)
 	{
-		SET_PIN_DIRECTION(PORTB_T, SS, OUT);
-		SET_PIN_DIRECTION(PORTB_T, MOSI, OUT);
-		SET_PIN_DIRECTION(PORTB_T, MISO, IN);
-		SET_PIN_DIRECTION(PORTB_T, SCK, OUT);
-
-		//set ss high
-		SET_BIT(*(volatile uint8*)PORTB_T,SS);
-
-		 //Enable the SPI,master mode ,Set the clock speed to F/16
-		SET_BIT(SPCR, SPE);
-		SET_BIT(SPCR, SPR0);
-		SET_BIT(SPCR, MSTR);
-
+		case SPI_Enable:
+		SPCR |=(1<<SPE);
+		
+		break;
+		
+		case SPI_Disable:
+		SPCR &=~(1<<SPE);
+		break;
+		default:
+		break;
 	}
-	else
+	switch (SPI_Object->spi_Select_Mode)
 	{
-		SET_PIN_DIRECTION(PORTB_T, SS, IN);
-		SET_PIN_DIRECTION(PORTB_T, MOSI, IN);
-		SET_PIN_DIRECTION(PORTB_T, MISO, OUT);
-		SET_PIN_DIRECTION(PORTB_T, SCK, IN);
-
-		 //Enable the SPI,Slave
-		SET_BIT(SPCR, SPE);
+		case SPI_Master_Mode:
+		SPCR |=(1<<MSTR);
+		SET_PIN_DIRECTION(PORTB_T,4,OUT);
+		SET_PIN_DIRECTION(PORTB_T,5,OUT);
+		SET_PIN_DIRECTION(PORTB_T,6,OUT);
+		SET_PIN_DIRECTION(PORTB_T,7,OUT);
+		break;
+		
+		case SPI_Slave_Mode:
+		SPCR &=~(1<<MSTR);
+		SET_PIN_DIRECTION(PORTB_T,6,IN);
+		break;
+		default:
+		break;
 	}
-
+	
+	switch (SPI_Object->spi_Clck_Mode)
+	{
+		case SPI_Clock_Mode0:
+		
+		break;
+		case SPI_Clock_Mode1:
+		SPCR |=(1<<CPHA);
+		break;
+		case SPI_Clock_Mode2:
+		SPCR |=(1<<CPOL);
+		break;
+		case SPI_Clock_Mode3:
+		SPCR |=(1<<CPHA)|(1<<CPOL);
+		break;
+	}
+	switch (SPI_Object->spi_Clock_Freq)
+	{
+		case FCPU_By2:
+		SPSR |=(1<<SPI2X);
+		break;
+		case FCPU_By4:
+		
+		break;
+		case FCPU_By8:
+		SPSR |=(1<<SPI2X);
+		SPCR |=(1<<SPR0);
+		break;
+		case FCPU_By16:
+		SPCR |=(1<<SPR0);
+		break;
+		case FCPU_By32:
+		SPSR |=(1<<SPI2X);
+		SPCR |=(1<<SPR1);
+		break;
+		case FCPU_By64:
+		SPSR |=(1<<SPI2X);
+		SPCR |=(1<<SPR0);
+		SPCR |=(1<<SPR1);
+		break;
+		case FCPU_By128:
+		SPCR |=(1<<SPR0);
+		SPCR |=(1<<SPR1);
+		break;
+		
+		default:
+		break;
+	}
+	
+	switch (SPI_Object->spi_data_order)
+	{
+		case SPI_Msb:
+		SPCR &=~(1<<DORD);
+		break;
+		case SPI_Lsb:
+		SPCR |=(1<<DORD);
+		break;
+	}
+	switch (SPI_Object->spi_Interrupt)
+	{
+		case SPI_Intrrupt_Enable:
+		SPCR |=(1<<SPIE);
+		break;
+		
+		case SPI_Intrrupt_Disable:
+		SPCR &=~(1<<SPIE);
+		break;
+	}
+	
 }
-void Spi_Mstr_Send(uint8 data)
+uint8 SPI_ReadWrite_ByteBlocking(uint8 data)
 {
 	SPDR = data;
-	while(!(SPSR & (1<<SPIF)));
+	while ((SPSR &(1<<SPIF))==0);
+	return SPDR;
 }
-uint8 Spi_Mstr_Recieve()
-{
-	SPDR = 0xFF;
-	while(!(SPSR & (1<<SPIF)));
-	return(SPDR);
-
-}
-
-
-void Spi_Slave_Send(uint8 data)
-{
-
-}
-uint8 Spi_Slave_Recieve()
-{
-	while(!(SPSR & (1<<SPIF)));	/* Wait till reception complete */
-	return(SPDR);			/* Return received data */
-}
+uint8 SPI_ReadWrite_ByteNonBlocking(uint8 data);
